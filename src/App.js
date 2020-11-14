eed14dcf669a9608044b0d9576a7774e8d37f07b
isiagi <isiagigeofrey0@gmail.com>
2020-11-15 00:40:21 +0300

    ft(home):add register and login buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home/Home"
+
 
 
 function App() {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom"
+import Button from "react-bootstrap/Button";
+import Login from "./firstSection/Login";
+import Register from "./firstSection/Register";
+import "./Home.css";
 
 export class Home extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <nav>
+          <h3>what do you need</h3>
+          <ul>
+           <Link to="/register"><Button>Register</Button></Link>
 
-    render() {
-        return (
-            <div>
-                <h1>Heyy</h1>
-            </div>
-        )
-    }
+            <Link to="/login"><Button>Login</Button></Link>
+          </ul>
+        </nav>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register}/>
+      </div>
+      </Router>
+    );
+  }
 }
 
-export default Home
+export default Home;
